b84bbbf64fb45262a3851b9ffb34cc229cb61612
mehmetyavuzz <mehmetyavuz@subu.edu.tr>
2021-09-01 17:27:56 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'quiz.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _qIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s your fav color?',
+      'answers': ['blue', 'red', 'green', 'white'],
+    },
+    {
+      'questionText': 'What\'s your fav animal?',
+      'answers': ['dog', 'cat', 'horse', 'bear'],
+    },
+    {
+      'questionText': 'What\'s your fav sport?',
+      'answers': ['football', 'baseball', 'basketball', 'ballball'],
+    }
+  ];
+
+  void _chooseAnswer() {
+    setState(() {
+      _qIndex++;
+    });
+    print(_qIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ssss'),
+        ),
+        body: _qIndex < _questions.length
+            ? Quiz(_chooseAnswer, _questions)
+            : Center(
+                child: Text('Done !'),
+              ),
+      ),
+    );
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'question.dart';
+import 'aswer.dart';
+
+class Quiz extends StatelessWidget {
+  // final List<Map<String, Object>> questions;
+  // final int questionIndex;
+  // final Function answerQuestion;
+
+  // Quiz({this.questions, this.});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(_questions[_qIndex]['questionText'].toString()),
+        ...(_questions[_qIndex]['answers'] as List<String>).map((answer) {
+          return Answer(_chooseAnswer, answer);
+        }).toList()
+      ],
+    );
+  }
+}
